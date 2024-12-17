90ea51dffe035f43cf1e50b7a0fb5e328912778c
Dmitry Blinov <dblinov76@gmail.com>
2024-12-17 17:15:07 +0200

    Initial anchor template

---
+++ programs/solana-proxy/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+declare_id!("H8xZNU5ZNAySjGH2yfVWA6V5McxjZZkXN9j6CvC3evko");
+
+#[program]
+pub mod solana_proxy {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
